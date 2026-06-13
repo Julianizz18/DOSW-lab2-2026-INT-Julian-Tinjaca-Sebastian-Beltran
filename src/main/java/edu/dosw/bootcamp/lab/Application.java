62301d4ae6528f104b5bd47b9bcce198be37d3b0
unknown <julian.tinjaca-c@labinfo.is.escuelaing.edu.co>
2026-06-13 10:56:02 -0500

    Empezamos lab

---
+++ src/main/java/edu/dosw/bootcamp/lab/Application.java
@@ -0,0 +1,10 @@
+package main.java.edu.dosw.bootcamp.lab;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(
+                "Proyecto Maven configurado y corriendo correctamente"
+        );
+    }
+}
+
